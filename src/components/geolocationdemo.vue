7b6a45624f37413141e9be0b016eb3b0a383ccfe
olatoc <44041435+olatoc@users.noreply.github.com>
2021-04-11 21:53:39 -0500

    add src and public

---
+++ src/components/geolocationdemo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <button @click="getLocation"> Allow location </button>
+        <h1 v-if="location"> {{location.coords.latitude}}, {{location.coords.longitude}} </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "geolocationdemo",
+    props: ['pos'],
+    created: function() {
+        //do we support geolocation
+        // if(!("geolocation" in navigator)) {
+        //     this.errorStr = 'Geolocation is not available.';
+        //     return;
+        // }
+    },
+    data: function() {
+        return {
+            location: null,
+            gettingLocation: false,
+            errorStr:null
+        }
+    },
+    methods: {
+        getLocation: function() {
+            navigator.geolocation.getCurrentPosition(pos => {
+                this.location = pos;
+            })
+        }
+    }
+}
+</script>
---
+++ src/views/Geolocation.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Geolocation">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Geolocation demo</h1>
+    <geolocationdemo></geolocationdemo>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import geolocationdemo from '@/components/geolocationdemo.vue'
+
+export default {
+  name: 'Geolocation',
+  components: {
+    geolocationdemo
+  }
+}
+</script>
