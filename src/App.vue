98773eeffd2d815f3c841a0480f3150b42156ea9
Debobani Biswas <biswa065@umn.edu>
2021-04-12 16:13:11 -0500

    remove user and about page

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,6 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      
-      <router-link to="/user">user</router-link> |
       <router-link to="/geolocationdemo">Geolocation</router-link> |
       <router-link to="/audiorecorder"> Audio Recorder</router-link> |
       <router-link to="/localnotifications"> Local Notifications</router-link> 
