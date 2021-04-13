10859d13cb8af1e06113c68306e91f34d043ca00
Debobani Biswas <biswa065@umn.edu>
2021-04-13 13:59:18 -0500

    empty state for demo

--- src/components/localnotification.vue
+++ src/components/localnotification.vue
@@ -1,12 +1,12 @@
 <template>
 <div>
     <p>Message:</p>
-    <input v-model="newWord" />
+    <input v-model="newMessage" />
     <p>Reminder Time (ms):</p>
     <input v-model="time" />
     <br>
     <br>
-    <button @click="addWord" > Set Notification </button>
+    <button @click="sendNotification" > Set Notification </button>
 </div>
 </template>
 <script>
@@ -14,40 +14,10 @@ export default {
     name: "localnotifications",
     data: function() {
         return {
-            newWord: "",
-            time: ""
         }
     },
     methods: {
-        addWord: function() {
-            if (!('Notification' in window)) {
-                alert('Notification API not supported!');
-                return;
-            }
-            Notification.requestPermission(function () {
-            });
-            var start = new Date().getTime();
-            for (var i = 0; i < 1e7; i++) {
-                if ((new Date().getTime() - start) > this.time){
-                break;
-                }
-            }
-            try {
-                var notification;
-                if(this.newWord == "") {
-                    notification = new Notification("Hi there - non-persistent!");
-                } else {
-                   notification= new Notification(this.newWord);
-                }
-                notification.onclick = function(event) {
-                    event.preventDefault();
-                    // window.open('http://www.google.com', '_blank');
-                    window.open('/', '_blank');
-                    // window.open(window.location.href, '_blank');
-                }
-            } catch (err) {
-                alert('Notification API error: ' + err);
-            }
+        sendNotification: function() {
         }
     }
 }
